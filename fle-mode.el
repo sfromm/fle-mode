7aba83d072050de11a1dcff3c3a7323e742d7960
Stephen Fromm <stephenf@nero.net>
2024-04-15 18:55:39 -0700

    bump version to 0.2

--- fle-mode.el
+++ fle-mode.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/sfromm/fle-mode
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: fle
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is not part of GNU Emacs
 ;;
@@ -53,7 +53,7 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
 
 ;; Constants
 
-(defconst fle-mode-version "0.1" "Version of `fle-mode'.")
+(defconst fle-mode-version "0.2" "Version of `fle-mode'.")
 
 (defconst fle-supported-bands
   '("2190m" "630m" "560m" "160m" "80m" "60m" "40m" "30m" "20m" "17m" "15m"
