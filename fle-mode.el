584a9539c8b41fed2847f7e29902e44c40091a4e
Stephen Fromm <stephenf@nero.net>
2025-01-31 16:18:47 -0800

    lots of work in fle-mode.el
    Rearrange various pieces
    Update regexes to use group syntax
    Add regexes to parsing FLE, including WWFF and RST reports
    Add functions to parse a FLE buffer and return a QSO list.

--- fle-mode.el
+++ fle-mode.el
@@ -59,9 +59,9 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
   :prefix "fle-"
   :group 'fle-mode)
 
-
 
 ;;; Faces
+
 (defface fle-callsign-face '((t (:inherit font-lock-keyword-face)))
   "Face for callsigns in FLE and related modes."
   :group 'fle-faces)
@@ -102,92 +102,6 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
   "Face for commented text in FLE and related modes."
   :group 'fle-faces)
 
-
-;;; Constants
-
-(defconst fle-mode-version "0.5" "Version of `fle-mode'.")
-
-(defconst fle-supported-bands
-  '("2190m" "630m" "560m" "160m" "80m" "60m" "40m" "30m" "20m" "17m" "15m"
-    "12m" "10m" "6m" "4m" "2m" "1.25m" "70cm" "33cm" "23cm" "13cm" "9cm"
-    "6cm" "3cm" "1.25cm" "6mm" "4mm" "2.5mm" "2mm" "1mm")
-  "Supported bands in FLE.")
-
-(defconst fle-supported-modes
-  '("CW" "SSB" "AM" "FM" "RTTY" "FT8" "PSK" "JT65" "JT9" "FT4" "JS8" "ARDOP"
-    "ATV" "C4FM" "CHIP" "CLO" "CONTESTI" "DIGITALVOICE" "DOMINO" "DSTAR" "FAX"
-    "FSK441" "HELL" "ISCAT" "JT4" "JT6M" "JT44" "MFSK" "MSK144" "MT63" "OLIVIA"
-    "OPERA" "PAC" "PAX" "PKT" "PSK2K" "Q15" "QRA64" "ROS" "RTTYM" "SSTV" "T10"
-    "THOR" "THRB" "TOR" "V4" "VOI" "WINMOR" "WSPR")
-  "Supported modes in FLE.")
-
-(defconst fle-date-regex
-  (rx bol (0+ space) "date" space (repeat 4 digit) "-" (repeat 2 digit) "-" (repeat 2 digit))
-  "Regular expression for FLE date format.")
-
-(defconst fle-hour-min-regex
-  (rx (or bol space) (or (repeat 4 digit) (repeat 2 digit)) space)
-  "Regular expression for FLE hour and minute format.")
-
-;; A collection of regexs to match callsigns
-;;   https://regex101.com/library/6QhGuD
-;;   https://regex101.com/library/uP6xD2
-(defconst fle-callsign-regex
-  (rx
-   (0+ alnum "/")
-   (repeat 1 3 alpha) (1+ digit) (repeat 0 3 alpha)
-   (0+ "/" alnum))
-  "Regular expression for a callsign.")
-
-(defconst fle-operator-regex
-  (rx "@" (1+ alnum))
-  "Regular expression for operator name.")
-
-(defconst fle-frequency-regex
-  (rx (repeat 1 3 digit) "." (repeat 3 digit))
-  "Regular expression for operating frequency.")
-
-;; example #JN49
-(defconst fle-gridlocator-regex
-  (rx (zero-or-one "#")  (repeat 2 alpha) (repeat 2 digit) (repeat 0 2 alnum))
-  "Regular expression for grid locator.")
-
-;; example w6/ct-226
-(defconst fle-sota-regex
-  (rx (repeat 1 3 alnum) "/" (repeat 2 alnum) "-" (repeat 3 digit))
-  "Regular expression for SOTA summit ID.")
-
-;; example DA-1234
-(defconst fle-pota-regex
-  (rx (repeat 1 3 alnum) "-" (repeat 4 5 digit))
-  "Regular expression for POTA park ID.")
-
-(defconst fle-qso-comment-regex
-  (rx "<" (0+ space) (1+ print) (0+ space) ">")
-  "Regular expression for QSO comment syntax.")
-
-(defconst fle-qso-remark-regex
-  (rx "{" (0+ space) (1+ print) (0+ space) "}")
-  "Regular expression for QSO remark syntax.")
-
-(defconst fle-comment-regex
-  (rx bol "#" (0+ print))
-  "Regular expression for syntax commenting a line.")
-
-(defconst fle-supported-bands-regex
-  (regexp-opt fle-supported-bands 'words)
-  "Regular expressions for supported bands in FLE.")
-
-(defconst fle-supported-modes-regex
-  (regexp-opt fle-supported-modes 'words)
-  "Regular expressions for supported modes in FLE.")
-
-(defconst fle-supported-keywords-regex
-  (regexp-opt
-   '("mycall" "mygrid" "operator" "qslmsg" "nickname"
-     "mywwff" "wwff" "mysota" "sota" "mypota" "pota" ) 'words)
-  "Regular expressions for FLE headers and keywords.")
-
 (defvar fle-callsign-face 'fle-callsign-face
   "Face for callsigns in FLE and related modes.")
 
@@ -218,6 +132,118 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
 (defvar fle-comment-face 'fle-comment-face
   "Face for commented text in FLE and related modes.")
 
+
+;;; Constants
+
+(defconst fle-mode-version "0.5" "Version of `fle-mode'.")
+
+(defvar fle-buffer-name
+  "*FLE*"
+  "Workspace buffer for FLE and FLEcli messages.")
+
+(defvar fle-qrz-query-url "https://www.qrz.com/db/"  "URL to query QRZ for a call-sign.")
+
+(defvar fle-pota-park-url "https://pota.app/#/park/" "URL to query QRZ for a call-sign.")
+
+(defconst fle-supported-bands
+  '("2190m" "630m" "560m" "160m" "80m" "60m" "40m" "30m" "20m" "17m" "15m"
+    "12m" "10m" "6m" "4m" "2m" "1.25m" "70cm" "33cm" "23cm" "13cm" "9cm"
+    "6cm" "3cm" "1.25cm" "6mm" "4mm" "2.5mm" "2mm" "1mm")
+  "Supported bands in FLE.")
+
+(defconst fle-supported-modes
+  '("CW" "SSB" "AM" "FM" "RTTY" "FT8" "PSK" "JT65" "JT9" "FT4" "JS8" "ARDOP"
+    "ATV" "C4FM" "CHIP" "CLO" "CONTESTI" "DIGITALVOICE" "DOMINO" "DSTAR" "FAX"
+    "FSK441" "HELL" "ISCAT" "JT4" "JT6M" "JT44" "MFSK" "MSK144" "MT63" "OLIVIA"
+    "OPERA" "PAC" "PAX" "PKT" "PSK2K" "Q15" "QRA64" "ROS" "RTTYM" "SSTV" "T10"
+    "THOR" "THRB" "TOR" "V4" "VOI" "WINMOR" "WSPR")
+  "Supported modes in FLE.")
+
+(defconst fle-date-regex
+  (rx bol (0+ space) "date" space (group (repeat 4 digit) "-" (repeat 2 digit) "-" (repeat 2 digit)))
+  "Regular expression for FLE date format.")
+
+(defconst fle-hour-min-regex
+  (rx (or bol space) (group (or (repeat 4 digit) (repeat 2 digit))) space)
+  "Regular expression for FLE hour and minute format.")
+
+;; A collection of regexs to match callsigns
+;;   https://regex101.com/library/6QhGuD
+;;   https://regex101.com/library/uP6xD2
+(defconst fle-callsign-regex
+  (rx (group
+       (0+ alnum "/")
+       (repeat 1 3 alpha) (1+ digit) (repeat 0 3 alpha)
+       (0+ "/" alnum)))
+  "Regular expression for a callsign.")
+
+(defconst fle-operator-callsign-regex
+  (rx "operator" (1+ space) (regex fle-callsign-regex))
+  "Regular expression for operator callsign.")
+
+(defconst fle-mycall-callsign-regex
+  (rx "mycall" (1+ space) (regex fle-callsign-regex))
+  "Regular expression for mycall and callsign.")
+
+(defconst fle-operator-regex
+  (rx "@" (1+ alnum))
+  "Regular expression for operator name.")
+
+(defconst fle-frequency-regex
+  (rx (group (repeat 1 3 digit) "." (repeat 3 digit)))
+  "Regular expression for operating frequency.")
+
+;; example #JN49
+(defconst fle-gridlocator-regex
+  (rx (zero-or-one "#")  (group (repeat 2 alpha) (repeat 2 digit) (repeat 0 2 alnum)))
+  "Regular expression for grid locator.")
+
+;; example w6/ct-226
+(defconst fle-sota-regex
+  (rx (repeat 1 3 alnum) "/" (repeat 2 alnum) "-" (repeat 3 digit))
+  "Regular expression for SOTA summit ID.")
+
+;; example DA-1234
+(defconst fle-pota-regex
+  (rx (repeat 1 3 alnum) "-" (repeat 4 5 digit))
+  "Regular expression for POTA park ID.")
+
+;; example ONFF-0001
+(defconst fle-wwff-regex
+  (rx (repeat 1 2 alpha) "FF" "-" (repeat 4 5 digit))
+  "Regular expression for WWFF ID.")
+
+(defconst fle-rst-report-regex
+  (rx space (group (zero-or-one "-") (repeat 2 3 digit))
+      (one-or-more space) (group (zero-or-one "-") (repeat 2 3 digit)))
+  "Regular expression for RST report.")
+
+(defconst fle-qso-comment-regex
+  (rx "<" (0+ space) (1+ print) (0+ space) ">")
+  "Regular expression for QSO comment syntax.")
+
+(defconst fle-qso-remark-regex
+  (rx "{" (0+ space) (1+ print) (0+ space) "}")
+  "Regular expression for QSO remark syntax.")
+
+(defconst fle-comment-regex
+  (rx bol "#" (0+ print))
+  "Regular expression for syntax commenting a line.")
+
+(defconst fle-supported-bands-regex
+  (regexp-opt fle-supported-bands 'words)
+  "Regular expressions for supported bands in FLE.")
+
+(defconst fle-supported-modes-regex
+  (regexp-opt fle-supported-modes 'words)
+  "Regular expressions for supported modes in FLE.")
+
+(defconst fle-supported-keywords-regex
+  (regexp-opt
+   '("mycall" "mygrid" "operator" "qslmsg" "nickname"
+     "mywwff" "wwff" "mysota" "sota" "mypota" "pota" ) 'words)
+  "Regular expressions for FLE headers and keywords.")
+
 ;; For font-lock faces, see
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
 (defvar fle-font-lock-keywords
@@ -239,20 +265,19 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
    )
   "Font locking definitions for FLE mode.")
 
-(defvar fle-messages
-  "*FLE-Messages*"
-  "Buffer for FLE and FLEcli messages.")
-
-(defvar fle-qrz-query-url
-  "https://www.qrz.com/db/"
-  "URL to query QRZ for a call-sign.")
-
-(defvar fle-pota-park-url
-  "https://pota.app/#/park/"
-  "URL to query QRZ for a call-sign.")
-
 
 ;;; Mode commands
+
+(defun fle-get-buffer ()
+  "Get the FLE buffer."
+  (let ((buffer (get-buffer-create fle-buffer-name)))
+    buffer))
+
+(defun fle-erase-buffer ()
+  "Erase ip-query buffer."
+  (interactive)
+  (with-current-buffer (fle-get-buffer) (erase-buffer)))
+
 (defun fle-find-mycall ()
   "Find MYCALL and save in buffer variable."
   (interactive)
@@ -371,11 +396,108 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
      (buffer-file-name))))
 
 
+;;; Parse a FLE buffer
+(defun fle-parse-default-rst (mode rst)
+  "Return RST report a default for the MODE if RST is nil."
+  (cond (rst rst)
+        ((member mode '("CW" "RTTY" "PSK")) "599")
+        ((member mode '("SSB" "AM" "FM")) "59")
+        ((member mode '("JT65" "JT9" "JT6M" "JT4" "JT44"
+                        "FSK441" "FT8" "FT4" "ISCAT" "MSK144" "QRA64" "T10" "WSPR")) "-10")
+        (t "59")))
+
+(defun fle-parse-fle-buffer ()
+  "Parse FLE buffer and return representation of QSOs."
+  (interactive)
+  (let ((qsos nil)
+        (mycall fle-mycall)
+        (operator fle-mycall)
+        (mygrid fle-mygrid)
+        (mypota fle-mypota)
+        (mysota fle-mysota)
+        (mywwff fle-mywwff)
+        (date nil))
+    (save-excursion
+      (goto-char (point-min))
+      (while (not (eobp))
+        (let ((line (substring (thing-at-point 'line t) 0 -1))
+              (name nil)
+              (band nil)
+              (mode nil)
+              (freq nil)
+              (time nil)
+              (callsign nil)
+              (gridsquare nil)
+              (rst-sent nil)
+              (rst-rcvd nil))
+          (when (string-match fle-mycall-callsign-regex line)
+            (setq mycall (match-string 1 line)))
+          (when (string-match fle-operator-callsign-regex line)
+            (setq operator (match-string 1 line)))
+          (when (string-match (rx "mygrid" (1+ space) (regex fle-gridlocator-regex)) line)
+            (setq mygrid (match-string 1 line)))
+          (when (string-match (rx "mypota" (1+ space) (regex fle-pota-regex)) line)
+            (setq mypota (match-string 1 line)))
+          (when (string-match (rx "mysota" (1+ space) (regex fle-sota-regex)) line)
+            (setq mysota (match-string 1 line)))
+          (when (string-match (rx "mywwff" (1+ space) (regex fle-wwff-regex)) line)
+            (setq mywwff (match-string 1 line)))
+          (when (string-match fle-date-regex line)
+            (setq date (date-to-time (match-string 1 line))))
+          (when (string-match fle-hour-min-regex line)
+            (setq time (match-string 1 line)))
+          ;; look for callsign of operator made qso with
+          (when (and (not (string-match-p (rx bol (or "mycall" "mygrid" "operator")) line))
+                     (string-match fle-callsign-regex line))
+            (setq callsign (match-string 0 line)))
+          (when (string-match fle-frequency-regex line)
+            (setq freq (match-string 0 line)))
+          (when (string-match fle-supported-bands-regex line)
+            (setq band (match-string 1 line)))
+          (when (string-match fle-supported-modes-regex line)
+            (setq mode (match-string 1 line)))
+          (when (string-match fle-rst-report-regex line)
+            (setq rst-sent (match-string 1 line))
+            (setq rst-rcvd (match-string 2 line)))
+          (when (and (not (string-match-p "mygrid" line)) (string-match fle-gridlocator-regex line))
+            (setq gridsquare (match-string 1 line)))
+          (when (string-match fle-operator-regex line)
+            (setq name (match-string 1 line)))
+          (when callsign
+            (message "adding qso with %s from line %s" callsign line)
+            (setq qso (list (list 'STATION_CALLSIGN (upcase mycall))
+                            (list 'MY_GRIDSQUARE mygrid)
+                            (list 'OPERATOR (upcase operator))
+                            (list 'CALL (upcase callsign))
+                            (list 'GRIDSQUARE gridsquare)
+                            (list 'BAND band)
+                            (list 'MODE mode)
+                            (list 'FREQ freq)
+                            (list 'QSO_DATE (format-time-string "%Y%m%d" date))
+                            (list 'TIME_ON time)
+                            (list 'RST_SENT (fle-parse-default-rst mode rst-sent))
+                            (list 'RST_SENT (fle-parse-default-rst mode rst-rcvd))
+                            (list 'RST_RCVD rst-rcvd)))
+            (push qso qsos)))
+        (forward-line 1)))
+    qsos))
+
+(defun fle-parse-fle-path (path)
+  "Parse FLE data from PATH and return QSO data."
+  (interactive "fFile path: ")
+  (save-excursion
+    (fle-erase-buffer)
+    (pop-to-buffer (fle-get-buffer))
+    (insert-file-contents (expand-file-name path))
+    (fle-mode)
+    (fle-parse-fle-buffer)))
+
+
 ;;; Integration with FLEcli
 
 (defun fle-flecli-run-command (cmd)
   "Run FLEcli command CMD."
-  (shell-command cmd fle-messages))
+  (shell-command cmd fle-buffer-name))
 
 (defun fle-flecli-load ()
   "Load and validate a FLE log with FLEcli."
@@ -441,6 +563,12 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
   "Submap for QSO-related commands")
 (fset 'fle-mode-qso-map fle-mode-qso-map)
 
+(defvar fle-buffer-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "q") 'fle-buffer-exit)
+    map)
+  "Keymap for FLE work buffer.")
+
 (defvar fle-mode-map
   (let ((map (make-sparse-keymap)))
     ;; these bindings are a work-in-progress to see what feels right.
@@ -465,6 +593,8 @@ Mode for editing FLE (fast-log-entry) amatuer radio logging files."
   (set (make-local-variable 'indent-tabs-mode) nil)
   (set (make-local-variable 'fle-mygrid) nil)
   (set (make-local-variable 'fle-mypota) nil)
+  (set (make-local-variable 'fle-mysota) nil)
+  (set (make-local-variable 'fle-mywwff) nil)
   (setq imenu-case-fold-search nil)
   (set (make-local-variable 'imenu-generic-expression) fle-imenu-expression)
   (fle-find-mycall)
